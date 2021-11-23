3714fe5739d8d48d44ef78ea499b8775d9a053cc
BackRunner <dev@backrunner.top>
2021-11-23 22:18:48 +0800

    feat: finish injector

--- src/constants/host.ts
+++
@@ -1,2 +0,0 @@
-export const GOOGLEAPIS_HOST = 'fonts.googleapis.com';
-export const GSTATIC_HOST = 'fonts.gstatic.com';
--- src/injector.ts
+++ src/injector.ts
@@ -1,11 +1,29 @@
-import { UserPluginConfig } from "./types";
+import { UserPluginConfig } from './types';
+
+const GOOGLEAPIS_HOST = 'fonts.googleapis.com';
+const GSTATIC_HOST = 'fonts.gstatic.com';
 
 class GoogleFontInjector {
   config: UserPluginConfig;
   constructor(config: UserPluginConfig) {
     this.config = config;
   }
-  inject(html: string) {}
+  inject(html: string) {
+    const { fonts, proxy, display } = this.config;
+    const apisHost = proxy || GOOGLEAPIS_HOST;
+    const gstaticHost = proxy || GSTATIC_HOST;
+    const fontFamily = fonts.map((font) => {
+      const family = `${font.family.replace(/\s/g, '+')}`;
+      if (font.weight) {
+        return `${family}:wght@${font.weight.join(';')}`;
+      }
+      return family;
+    });
+    const preconnect = `<link rel="preconnect" href="https://${apisHost}" crossorigin><link rel="preconnect" href="https://${gstaticHost}" crossorigin>`;
+    const cssUrl = `https://${apisHost}/css2?family=${fontFamily.join('&family=')}&display=${display || 'swap'}`;
+    const cssTag = `<lint href="${cssUrl}" rel="stylesheet">`;
+    return html.replace('</head>', `${preconnect}${cssTag}</head>`);
+  }
 }
 
 export const getInjector = (config: UserPluginConfig) => {
